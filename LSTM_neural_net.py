d384a23fff480b06420fb84631c6df19ded73a33
Matthew <matthewtranov@gmail.com>
2018-04-01 21:42:47 -0400

    cleaned up data

--- LSTM_neural_net.py
+++ LSTM_neural_net.py
@@ -1,18 +1,12 @@
 import ast
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split as split
+
 
 from keras.layers import LSTM, Input, Dense
 from keras.models import Model
 
-from deap import base, creator, tools, algorithms
-from scipy.stats import bernoulli
-from bitstring import BitArray
-
-WINDOW_SIZE = 3
-
+WINDOW_SIZE = 5
 
 def get_data(file_path):
     '''turn file of lists into list of lists'''
@@ -66,14 +60,30 @@ def preprocess_data(price_data):
         next_change_list = []
         for i in range(len(stock) - WINDOW_SIZE - 1):
             # add the window to the list
-            price_tuples_list.append(np.array(stock[i:i + 3],ndmin =3))
+            price_tuples_list.append(np.array(stock[i:i + WINDOW_SIZE],ndmin =3))
             # add the next price to the list
-            next_change_list.append(np.array(stock[i + 3],ndmin =3))
+            next_change_list.append(np.array(stock[i + WINDOW_SIZE],ndmin =3))
         given_data.append(price_tuples_list)
         future_data.append(next_change_list)
     return given_data, future_data
     # output the data to use and the data to predict
 
+def fix_data(percent_change_data):
+    better_list = []
+    for stock_percent_change_data in percent_change_data:
+        should_delete = False
+        countains_non_zero = False
+        i = 0
+        while not should_delete and i < len(stock_percent_change_data):
+            if stock_percent_change_data[i] != 0:
+                countains_non_zero = True
+            if abs(stock_percent_change_data[i]) >= 0.1:
+                should_delete = True
+            i += 1
+        if not should_delete and countains_non_zero:
+            better_list.append(stock_percent_change_data)
+
+    return better_list
 
 def evaulate_fitness(data, model):
     pass
@@ -106,55 +116,55 @@ def data_creation():
 
 
 def neural_net(given_data, future_data):
-    x_train = given_data[:9000]
-    x_eval = given_data[9000:]
-    y_train = future_data[:9000]
-    y_eval = future_data[9000:]
+    x_train = given_data[:2000]
+    x_eval = given_data[2000:3000]
+    y_train = future_data[:2000]
+    y_eval = future_data[2000:3000]
 
     # tell keras to expect an unknown number of window size-dimensional vectors
-    inputs = Input( batch_shape=(1, 1, WINDOW_SIZE))
+    inputs = Input(batch_shape=(1, 1, WINDOW_SIZE))
     # creates LSTM to act as second layer of neural net
     # the output is one unit
     # the previous layer (inputs) is taken in as input
-    encoder = LSTM(10, return_sequences=True, stateful=True)(inputs)
+    encoder = LSTM(5, return_sequences=True, stateful=True)(inputs)
     # creates a final layer of the neural net to act as output
-    predictions = Dense(1, activation='linear')(encoder)
+    predictions = Dense(1, activation='sigmoid')(encoder)
     # use first and last layer of neural net as input and output
     model = Model(inputs=inputs, outputs=predictions)
     # configures the model for training
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     print('Train...')
-    # train of the data 15 times
+    # train on the data 15 times
     for epoch in range(15):
-        mean_train_loss = []
+        train_loss_list = []
         for i in range(len(x_train)):
+            if i % (len(x_train)//10) == 0:
+                print(i // (len(x_train)//10), '/10')
             for j in range(len(x_train[i])):
                 tr_loss = model.train_on_batch(x_train[i][j], y_train[i][j])
-                mean_train_loss.append(tr_loss)
+                train_loss_list.append(tr_loss)
             model.reset_states()
-
-        print('loss training = {}'.format(np.mean(mean_train_loss)))
+        prediction = model.predict(np.array([-0.0028, -0.0029, -0.0028, -0.0007, 0.0014],ndmin =3))
+        print(prediction)
+        print('loss on training data = {}'.format(np.mean(train_loss_list)))
         print('___________________________________')
 
-        mean_te_acc = []
-        mean_te_loss = []
+        test_loss_list = []
         for i in range(len(x_eval)):
+            if i % (len(x_eval) // 10) == 0:
+                print(i // (len(x_eval)//10), '/10')
             for j in range(len(x_eval[i])):
                 te_loss = model.test_on_batch(x_eval[i][j], y_eval[i][j])
-                mean_te_loss.append(te_loss)
+                test_loss_list.append(te_loss)
             model.reset_states()
 
-            # for j in range(max_len):
-            #     y_pred = model.predict_on_batch(
-            #         np.expand_dims(np.expand_dims(X_test[i][j], axis=1),
-            #                        axis=1))
-            # model.reset_states()
-
-        print('accuracy testing = {}'.format(np.mean(mean_te_acc)))
-        print('loss testing = {}'.format(np.mean(mean_te_loss)))
+        print('loss on test data = {}'.format(np.mean(test_loss_list)))
         print('___________________________________')
 
-percent_lists = get_data('percent_change_data.txt')
+percent_lists = get_data('fixed_percent_change_data.txt')
+#fixed_list = fix_data(percent_lists)
+#write_data(fixed_list, 'fixed_percent_change_data.txt')
 X_data, Y_data = preprocess_data(percent_lists)
 neural_net(X_data, Y_data)
+
